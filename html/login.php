db9affc985750ce080633f416575549db7d82d77
yuvelq <oa4doa@gmail.com>
2022-04-23 22:56:56 -0500

    fix typing error

--- html/login.php
+++ html/login.php
@@ -79,7 +79,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
   <img class="img-top" src="img/logo.png?random=323527528432525.24234" alt="">
   <h2><?= REPORT_NAME;?></h2>
-  <div><?php include_once "buttons.html"; ?></div>
+  <div><?php include_once "buttons.php"; ?></div>
   <fieldset class="login selfserv">
     <legend><b>&nbsp;.: Login Form :.&nbsp;</b></legend>
     <script>
