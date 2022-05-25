e072412ca00660c169884457486df527a105f99f
yuvelq <oa4doa@gmail.com>
2022-05-24 20:14:29 -0500

    Add MySQL support

--- mon_db.py
+++ mon_db.py
@@ -20,6 +20,8 @@
 
 import logging
 from json import loads as jloads
+from select import select
+import sys
 
 from twisted.enterprise import adbapi
 from twisted.internet.defer import inlineCallbacks, returnValue
@@ -47,88 +49,105 @@ def sec_time(_time):
         return f'{seconds}s'
 
 
-class MoniDB:
-    def __init__(self, db_name):
-        self.db = adbapi.ConnectionPool("sqlite3", db_name, check_same_thread=False)
 
+class MoniDB:
+    def __init__(self, host, user, psswd, db_name):
+        self.db = adbapi.ConnectionPool("MySQLdb", host, user, psswd, db_name)
+
+    def test_db(self):
+        return self.db.runQuery("select 1")
+            
     @inlineCallbacks
     def create_tables(self):
         try:
             def create_tbl(txn):
+                txn.execute(''' CREATE TABLE IF NOT EXISTS Clients(
+                            int_id INT UNIQUE PRIMARY KEY NOT NULL,
+                            dmr_id TINYBLOB NOT NULL,
+                            callsign VARCHAR(10) NOT NULL,
+                            host VARCHAR(15),
+                            options VARCHAR(100),
+                            opt_rcvd TINYINT(1) DEFAULT False NOT NULL,
+                            mode TINYINT(1) DEFAULT 4 NOT NULL,
+                            logged_in TINYINT(1) DEFAULT False NOT NULL,
+                            modified TINYINT(1) DEFAULT False NOT NULL,
+                            psswd BLOB(256),
+                            last_seen INT NOT NULL) CHARSET=utf8mb4''')
+
                 txn.execute('''CREATE TABLE IF NOT EXISTS talkgroup_ids (
                             id INT PRIMARY KEY UNIQUE NOT NULL, 
-                            callsign VARCHAR(255) NOT NULL)''')
+                            callsign VARCHAR(255) NOT NULL) CHARSET=utf8mb4''')
 
                 txn.execute('''CREATE TABLE IF NOT EXISTS subscriber_ids (
                             id INT PRIMARY KEY UNIQUE NOT NULL,
                             callsign VARCHAR(255) NOT NULL,
-                            name VARCHAR(255) NOT NULL)''')
+                            name VARCHAR(255) NOT NULL) CHARSET=utf8mb4''')
 
                 txn.execute('''CREATE TABLE IF NOT EXISTS peer_ids (
                             id INT PRIMARY KEY UNIQUE NOT NULL,
-                            callsign VARCHAR(255) NOT NULL)''')
+                            callsign VARCHAR(255) NOT NULL) CHARSET=utf8mb4''')
 
                 txn.execute('''CREATE TABLE IF NOT EXISTS last_heard (
-                            date_time TEXT NOT NULL,
-                            qso_time DECIMAL(3,2),
+                            date_time DATETIME NOT NULL,
+                            qso_time DECIMAL(5,2),
                             qso_type VARCHAR(20) NOT NULL,
                             system VARCHAR(50) NOT NULL,
                             tg_num INT NOT NULL,
-                            dmr_id INT PRIMARY KEY UNIQUE NOT NULL)''')
+                            dmr_id INT PRIMARY KEY UNIQUE NOT NULL) CHARSET=utf8mb4''')
 
                 txn.execute('''CREATE TABLE IF NOT EXISTS lstheard_log (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            date_time TEXT NOT NULL,
-                            qso_time DECIMAL(3,2),
+                            date_time DATETIME NOT NULL,
+                            qso_time DECIMAL(5,2),
                             qso_type VARCHAR(20) NOT NULL,
                             system VARCHAR(50) NOT NULL,
                             tg_num INT NOT NULL,
-                            dmr_id INT NOT NULL)''')
+                            dmr_id INT NOT NULL) CHARSET=utf8mb4''')
 
                 txn.execute('''CREATE TABLE IF NOT EXISTS tg_count (
-                            date TEXT NOT NULL,
+                            date DATETIME NOT NULL,
                             tg_num INT PRIMARY KEY NOT NULL,
                             qso_count INT NOT NULL,
-                            qso_time DECIMAL(4,2) NOT NULL)''')
+                            qso_time DECIMAL(5,2) NOT NULL) CHARSET=utf8mb4''')
 
                 txn.execute('''CREATE TABLE IF NOT EXISTS user_count (
-                            date TEXT NOT NULL,
+                            date DATETIME NOT NULL,
                             tg_num INT NOT NULL,
                             dmr_id INT NOT NULL,
-                            qso_time DECIMAL(4,2) NOT NULL,
-                            UNIQUE(tg_num, dmr_id))''')
+                            qso_time DECIMAL(5,2) NOT NULL,
+                            UNIQUE(tg_num, dmr_id)) CHARSET=utf8mb4''')
 
             yield self.db.runInteraction(create_tbl)
             logger.info("Tables created successfully.")
 
         except Exception as err:
-            logger.error(f"create_tables: {err}, {type(err)}")
+            logger.error(f"create_tables: {err}.")
 
     @inlineCallbacks
     def populate_tbl(self, table, lst_data, wipe_tbl, _file):
         try:
             def populate(txn, wipe_tbl):
                 if table == "talkgroup_ids":
-                    stm = "INSERT OR IGNORE INTO talkgroup_ids VALUES (?, ?)"
-                    w_stm = "DELETE FROM talkgroup_ids"
+                    stm = "INSERT IGNORE INTO talkgroup_ids VALUES (%s, %s)"
+                    w_stm = "TRUNCATE TABLE talkgroup_ids"
                 elif table == "subscriber_ids":
-                    stm = stm = "INSERT OR IGNORE INTO subscriber_ids VALUES (?, ?, ?)"
-                    w_stm = "DELETE FROM subscriber_ids"
+                    stm = stm = "INSERT IGNORE INTO subscriber_ids VALUES (%s, %s, %s)"
+                    w_stm = "TRUNCATE TABLE subscriber_ids"
                 elif table == "peer_ids":
-                    stm = "INSERT OR IGNORE INTO peer_ids VALUES (?, ?)"
-                    w_stm = "DELETE FROM peer_ids"
+                    stm = "INSERT IGNORE INTO peer_ids VALUES (%s, %s)"
+                    w_stm = "TRUNCATE TABLE peer_ids"
 
                 if wipe_tbl:
                     txn.execute(w_stm)
 
-                result = txn.executemany(stm, lst_data).rowcount
-                if result > 0:
-                    logger.info(f"{result} entries added to: {table} table from: {_file}")
+                txn.executemany(stm, lst_data)
+                
+                if txn.rowcount > 0:
+                    logger.info(f"{txn.rowcount} entries added to: {table} table from: {_file}")
 
             yield self.db.runInteraction(populate, wipe_tbl)
 
         except Exception as err:
-            logger.error(f"populate: {err}, {type(err)}")
+            logger.error(f"populate_tbl({_file}): {err}.")
 
     @inlineCallbacks
     def table_count(self, _table):
@@ -147,36 +166,36 @@ class MoniDB:
                 returnValue(None)
 
         except Exception as err:
-            logger.error(f"table_count: {err}, {type(err)}")
+            logger.error(f"table_count: {err}.")
 
     @inlineCallbacks
     def ins_lstheard(self, qso_time, qso_type, system, tg_num, dmr_id):
         try:
             yield self.db.runOperation(
-                "INSERT OR REPLACE INTO last_heard VALUES (datetime('now', 'localtime'), ?, ?, ?, ?, ?)",
+                "REPLACE INTO last_heard VALUES (now(), %s, %s, %s, %s, %s)",
                 (qso_time, qso_type, system, tg_num, dmr_id))
 
         except Exception as err:
-            logger.error(f"ins_lstheard: {err}, {type(err)}")
+            logger.error(f"ins_lstheard: {err}.")
 
     @inlineCallbacks
     def ins_lstheard_log(self, qso_time, qso_type, system, tg_num, dmr_id):
         try:
             yield self.db.runOperation(
                 '''INSERT INTO lstheard_log (date_time, qso_time, qso_type, system, tg_num, dmr_id)
-                VALUES(datetime('now', 'localtime'), ?, ?, ?, ?, ?)''',
+                VALUES(now(), %s, %s, %s, %s, %s)''',
                 (qso_time, qso_type, system, tg_num, dmr_id))
 
         except Exception as err:
-            logger.error(f"ins_lstheard_log: {err}, {type(err)}")
+            logger.error(f"ins_lstheard_log: {err}.")
 
     @inlineCallbacks
     def slct_2dict(self, _id, _table):
         try:
             if _table == "subscriber_ids":
-                stm = "SELECT * FROM subscriber_ids WHERE id = ?"
+                stm = "SELECT * FROM subscriber_ids WHERE id = %s"
             elif _table == "talkgroup_ids":
-                stm = "SELECT * FROM talkgroup_ids WHERE id = ?"
+                stm = "SELECT * FROM talkgroup_ids WHERE id = %s"
 
             result = yield self.db.runQuery(stm, (_id,))
             if result:
@@ -185,26 +204,26 @@ class MoniDB:
                 returnValue(None)
 
         except Exception as err:
-            logger.error(f"slct_2dict: {err}, {type(err)}")
+            logger.error(f"slct_2dict: {err}.")
 
     @inlineCallbacks
     def slct_2render(self, _table, _row_num):
         try:
             if _table == "last_heard":
-                stm = '''SELECT date_time, qso_time, qso_type, system, tg_num,
+                stm = '''SELECT CONVERT(date_time, CHAR), qso_time, qso_type, system, tg_num,
                     (SELECT callsign FROM talkgroup_ids WHERE id = tg_num), dmr_id,
                     (SELECT json_array(callsign, name) FROM subscriber_ids WHERE id = dmr_id)
-                    FROM last_heard ORDER BY date_time DESC LIMIT ?'''
+                    FROM last_heard ORDER BY date_time DESC LIMIT %s'''
 
             elif _table == "lstheard_log":
-                stm = '''SELECT date_time, qso_time, qso_type, system, tg_num,
+                stm = '''SELECT CONVERT(date_time, CHAR), qso_time, qso_type, system, tg_num,
                     (SELECT callsign FROM talkgroup_ids WHERE id = tg_num), dmr_id,
                     (SELECT json_array(callsign, name) FROM subscriber_ids WHERE id = dmr_id)
-                    FROM lstheard_log ORDER BY date_time DESC LIMIT ?'''
+                    FROM lstheard_log ORDER BY date_time DESC LIMIT %s'''
 
             result = yield self.db.runQuery(stm, (_row_num,))
+            tmp_lst = []
             if result:
-                tmp_lst = []
                 for row in result:
                     if row[7]:
                         r_lst = list(row)
@@ -212,46 +231,46 @@ class MoniDB:
                         tmp_lst.append(tuple(r_lst))
                     else:
                         tmp_lst.append(row)
-                returnValue(tmp_lst)
-            else:
-                returnValue(None)
-
+            returnValue(tmp_lst)
+            
         except Exception as err:
-            logger.error(f"slct_2render: {err}, {type(err)}")
+            logger.error(f"slct_2render: {err}.")
 
     @inlineCallbacks
     def clean_table(self, _table, _row_num):
         try:
             if _table == "last_heard":
-                stm = '''DELETE FROM last_heard WHERE dmr_id NOT IN
-                    (SELECT dmr_id FROM last_heard ORDER BY date_time DESC LIMIT ?)'''
+                stm = '''DELETE FROM last_heard WHERE date_time <= (SELECT date_time FROM (
+                    SELECT date_time FROM last_heard ORDER BY date_time DESC LIMIT 1 OFFSET %s)
+                    foo )'''
 
             elif _table == "lstheard_log":
-                stm = '''DELETE FROM lstheard_log WHERE id NOT IN
-                    (SELECT id FROM lstheard_log ORDER BY date_time DESC LIMIT ?)'''
+                stm = '''DELETE FROM lstheard_log WHERE date_time <= (SELECT date_time FROM (
+                    SELECT date_time FROM lstheard_log ORDER BY date_time DESC LIMIT 1 OFFSET %s)
+                    foo )'''
 
             yield self.db.runOperation(stm, (int(_row_num * 1.25),))
             logger.info(f"{_table} DB table cleaned successfully.")
 
         except Exception as err:
-            logger.error(f"clean_tables: {err}, {type(err)}")
+            logger.error(f"clean_tables: {err}.")
 
     @inlineCallbacks
     def ins_tgcount(self, _tg_num, _dmr_id, _qso_time):
         try:
             def db_actn(txn):
-                txn.execute('''INSERT INTO tg_count VALUES (date('now', 'localtime'), ?, 1, ?)
-                            ON CONFLICT (tg_num) DO UPDATE SET qso_time = qso_time + ?,
+                txn.execute('''INSERT INTO tg_count VALUES (CURDATE(), %s, 1, %s)
+                            ON DUPLICATE KEY UPDATE qso_time = qso_time + %s,
                             qso_count = qso_count + 1''', (_tg_num, _qso_time, _qso_time))
 
-                txn.execute('''INSERT INTO user_count VALUES(date('now', 'localtime'), ?, ?, ?)
-                            ON CONFLICT (tg_num, dmr_id) DO UPDATE SET qso_time = qso_time + ?''',
+                txn.execute('''INSERT INTO user_count VALUES(CURDATE(), %s, %s, %s)
+                            ON DUPLICATE KEY UPDATE qso_time = qso_time + %s''',
                             (_tg_num, _dmr_id, _qso_time, _qso_time))
 
             yield self.db.runInteraction(db_actn)
 
         except Exception as err:
-            logger.error(f"ins_tgcount: {err}, {type(err)}")
+            logger.error(f"ins_tgcount: {err}.")
 
     @inlineCallbacks
     def slct_tgcount(self, _row_num):
@@ -259,14 +278,14 @@ class MoniDB:
             rows = yield self.db.runQuery(
                 '''SELECT tg_num, ifnull(callsign, ''), qso_count, qso_time FROM tg_count
                 LEFT JOIN talkgroup_ids ON talkgroup_ids.id = tg_count.tg_num ORDER BY qso_time
-                DESC LIMIT ?''', (_row_num,))
+                DESC LIMIT %s''', (_row_num,))
             if rows:
                 res_lst = []
                 for tg_num, name, qso_c, qso_time in rows:
                     res = yield self.db.runQuery(
                         '''SELECT ifnull(callsign, "N0CALL") FROM user_count 
                         LEFT JOIN subscriber_ids ON subscriber_ids.id = user_count.dmr_id
-                        WHERE tg_num = ? ORDER BY qso_time DESC LIMIT 4''', (tg_num,))
+                        WHERE tg_num = %s ORDER BY qso_time DESC LIMIT 4''', (tg_num,))
 
                     res_lst.append((tg_num, name, qso_c, sec_time(qso_time), tuple([ite[0] for ite in res])))
                 returnValue(res_lst)
@@ -274,33 +293,43 @@ class MoniDB:
                 returnValue(None)
 
         except Exception as err:
-            logger.error(f"slct_tgcount: {err}, {type(err)}")
+            logger.error(f"slct_tgcount: {err}.")
 
     @inlineCallbacks
     def clean_tgcount(self):
         try:
             yield self.db.runOperation(
-                "DELETE FROM tg_count WHERE date IS NOT date('now', 'localtime')")
+                "DELETE FROM tg_count WHERE date != CURDATE()")
             yield self.db.runOperation(
-                "DELETE FROM user_count WHERE date IS NOT date('now', 'localtime')")
+                "DELETE FROM user_count WHERE date != CURDATE()")
 
             logger.info("TG Count tables cleaned successfully")
 
         except Exception as err:
-            logger.error(f"clean_tgcount: {err}, {type(err)}")
+            logger.error(f"clean_tgcount: {err}.")
 
 
 if __name__ == '__main__':
+    from argparse import ArgumentParser
     from twisted.internet import reactor
+    
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
 
-    logging.basicConfig(
-        level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    parser = ArgumentParser()
+    parser.add_argument("--create",
+                      action="store_true", dest="create_tbl",
+                      help="Create FDMR Monitor database tables")
+    args = parser.parse_args()
 
-    # Create an instance of MoniDB
-    test_db = MoniDB("mon.db")
+    if args.create_tbl:
+        # Create an instance of MoniDB
+        _db = MoniDB("localhost", "root", "", "test")
 
-    # Create tables in db
-    test_db.create_tables()
+        # Create tables in db
+        
+        reactor.callLater(1, _db.create_tables)
 
-    reactor.callLater(5, reactor.stop)
-    reactor.run()
+        reactor.callLater(3, reactor.stop)
+        reactor.run()
